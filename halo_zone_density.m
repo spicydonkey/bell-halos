f1b06c00cee404522b5ac449fadb028094381aa5
David <dk.shin1992@gmail.com>
2017-09-18 11:49:40 +1000

    update zonal density function: - skeleton for binning

--- halo_zone_density.m
+++ halo_zone_density.m
@@ -3,6 +3,7 @@ function nn=halo_zone_density(halo,azim,elev,verbose)
 % halo_zone_density(halo, verbose)
 % 
 % halo: num_shot x 1 cell-array of num_countsx3 double array (XYZ)
+    % halo should be correctly centered
 % azim: def grid points - array of azim angles
 % elev: def grid points - array of elev angles (meas from XY plane)
 %
@@ -13,12 +14,39 @@ function nn=halo_zone_density(halo,azim,elev,verbose)
     % meshgrid for azim-elev is ideal for surf plotting on sphere
 
 % XYZ --> spherical angle (2D)
-% set up grid of spherical angles + bin style + width
+nshot=size(halo,1);
+halo_sphpol=cell(nshot,1);
+for ii=1:nshot
+    this_halo=halo{ii};
+    halo_sphpol{ii}=zeros(size(this_halo,1),2);
+    [halo_sphpol{ii}(:,1),halo_sphpol{ii}(:,2)]=cart2sph(this_halo(:,1),this_halo(:,2),this_halo(:,3));
+end
+
+grid_thphi=[azim(:),elev(:)];
+ngrid=size(grid_thphi,1);       % number of grid points
+% method 1
+% bin style: simple counting bin with limits
+binwidth=deg2rad(10);   % some appropriate binning angle (rad)
+for ii=1:nshot
+    % set up grid of spherical angles + bin style + width
     % get effective counts at each grid point
-        % per shot
-    % evaluate normalised density (integrates to 1 around domain)
-        % NOTE: simple linspace sampling for azim,elev from limits doesn't
-        % uniformly sample the sphere.
+    % per shot
+    
+    this_halo_thphi=halo_sphpol{ii};
+    this_ncounts=size(this_halo_thphi,1);
+    
+    % for each grid-point evaluate angle difference
+    for jj=1:ngrid
+        this_thphi=grid_thphi(jj,:);  % this grid point
+        
+        this_psi=sphdiffangle(this_thphi(1),this_thphi(2),this_halo_thphi(:,1),this_halo_thphi(:,2));
+    end
+end
+% evaluate normalised density (integrates to 1 around domain)
+    % NOTE: simple linspace sampling for azim,elev from limits doesn't
+    % uniformly sample the sphere.
+
+
 % statistics
     % mean
     % sdev, serr (shot-to-shot)
