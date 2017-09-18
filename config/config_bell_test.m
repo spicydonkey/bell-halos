f75d512ef5e119b2d0dcf6f1cf7e9dada60c839f
David <dk.shin1992@gmail.com>
2017-09-18 16:40:17 +1000

    first attempt at loading Bell test halos

---
+++ config/config_bell_test.m
@@ -0,0 +1,95 @@
+% Configuration file for distinguishable_halos.m
+
+%%% FLAGS
+configs.flags.do_corr_analysis=0;
+configs.flags.force_all_stages=0;    % force all the stages to run (useful for debug)
+configs.flags.verbose=2;
+configs.flags.savedata=0;       % TODO - req'd currently since each stage passes data by save/load to disk
+configs.flags.archive_txy=0;        % archives loaded TXY as .mat file for future reuse
+configs.flags.graphics=1;       % toggle to control graphics/plotting options
+configs.flags.build_txy=1;
+
+%%% MISCELLANEOUS
+configs.misc.vel_z=9.8*0.416;    % atom free-fall vert v at detector hit for T-to-Z conversion;
+vz=configs.misc.vel_z;
+
+
+%% FILES
+configs.files.path='\\AMPLPC29\Users\TDC_user\ProgramFiles\my_read_tdc_gui_v1.0.1\dld_output\bell_progress_201709\bell_test_2\d';
+
+% WARNING: MODIFYING BELOW DIR SETTINGS ARE NOT RECOMMENDED
+configs.files.dir_data=fileparts(configs.files.path);    % fullpath to data directory
+configs.files.archive=fullfile(configs.files.dir_data,'archive');   % dir to archive folder
+configs.files.dirout=fullfile(configs.files.dir_data,'output');      % output directory (will be time-stamped)
+
+
+%% LOAD
+% version 1 - 10ns txy dead time
+% version 2 - 100ns txy dead time
+configs.load.version=2;         % TXY load stage version number
+
+% file ID and simple pass/fail
+configs.load.id=1:1000;         % file id numbers to use for analysis
+configs.load.mincount=0;         % min counts in window - 0 for no min
+configs.load.maxcount=Inf;          % max counts in window - Inf for no max
+
+% Detector/trap alignment
+configs.load.rot_angle=0.61;
+
+% TXY window - region of interest ( [] --> no crop )
+configs.load.window{1}=[2.442,2.482];      % T [s]
+configs.load.window{2}=[-35e-3,35e-3];    % X [m]
+configs.load.window{3}=[-35e-3,35e-3];    % Y [m]
+
+%% HALO
+%%% HALO PARAMS: BEC counts + oscillation removal for broad capture of halos
+configs.bec{1}.pos{1}=[10.0564,-3.4e-3,4.5e-3];   % approx condensate locations (z,x,y)
+configs.bec{1}.Rmax{1}=12e-3;      % max condensate sph radius
+configs.bec{1}.dR_tail{1}=1;     % BEC tail radial frac diff
+configs.bec{1}.pos{2}=[10.1099,-2.9e-3,4.6e-3];
+configs.bec{1}.Rmax{2}=12e-3;
+configs.bec{1}.dR_tail{2}=1;
+
+configs.halo{1}.R{1}=25e-3;     % estimated radius of halo
+configs.halo{1}.dR{1}=0.2;      % broad radial mask fractional width (in/out)
+
+configs.halo{1}.zcap=0.75;   % z-cutoff (fractional wrt radius)
+
+configs.bec{2}.pos{1}=[9.9820,-3.7e-3,3.3e-3];   % approx condensate locations (z,x,y)
+configs.bec{2}.Rmax{1}=12e-3;      % max condensate sph radius
+configs.bec{2}.dR_tail{1}=1;     % BEC tail radial frac diff
+configs.bec{2}.pos{2}=[10.0330,-3.0e-3,3.7e-3];
+configs.bec{2}.Rmax{2}=12e-3;
+configs.bec{2}.dR_tail{2}=1;
+
+configs.halo{2}.R{1}=20e-3;     % estimated radius of halo
+configs.halo{2}.dR{1}=0.2;      % broad radial mask fractional width (in/out)
+
+configs.halo{2}.zcap=0.75;   % z-cutoff (fractional wrt radius)
+
+% TODO - does boost need to be optimised for different g2 analysis?
+%   currently SINGLE boost applied to halo2 to obtain best g2_01_BB
+configs.halo{1}.boost=zeros(1,3);
+configs.halo{2}.boost=zeros(1,3);
+
+%% CORRELATION ANALYSIS
+% % 1) Single-halo Angular - m_J=0
+% configs.corr{1}.type.comp=1;           
+% configs.corr{1}.type.coord='angular';
+% configs.corr{1}.type.opt=[];
+% configs.corr{1}.lim=[0,0.2;0,pi];
+% configs.corr{1}.nBin=[21,401];
+% 
+% % 2) Single-halo cart CL - m_J=0
+% configs.corr{2}.type.comp=1;
+% configs.corr{2}.type.coord='cart';
+% configs.corr{2}.type.opt='CL';
+% configs.corr{2}.lim=0.2*repmat([0,1],[3,1]);
+% configs.corr{2}.nBin=11*[1,1,1];   % number of bins
+% 
+% % 3) Single-halo cart BB - m_J=0
+% configs.corr{3}.type.comp=1;
+% configs.corr{3}.type.coord='cart';
+% configs.corr{3}.type.opt='BB';
+% configs.corr{3}.lim=0.2*repmat([-1,1],[3,1]);
+% configs.corr{3}.nBin=11*[1,1,1];   % number of bins
---
+++ run_zonecount.m
@@ -0,0 +1,143 @@
+% mf=(1,1) collision analysis
+% Exmaple of single-halo capture
+
+clear all; clc; close all;
+
+%% set config
+% USER SET CONFIG!
+config_bell_test;
+
+verbose=configs.flags.verbose;
+
+
+%% load TXY
+txy=load_txy(configs.files.path,configs.load.id,...
+            configs.load.window,configs.load.mincount,configs.load.maxcount,...
+            configs.load.rot_angle,configs.flags.build_txy,verbose,configs.flags.graphics);
+        
+zxy=txy2zxy(txy);       % zxy
+
+
+%% capture each halo
+% configs
+col={'b','r'};
+
+% preallocate
+halo_k=cell(2,1);
+bec_cent=cell(2,1);
+halo_cent=cell(2,1);
+ecent=cell(2,1);
+erad=cell(2,1);
+evecs=cell(2,1);
+Nsc=cell(2,1);
+dk=cell(2,1);
+halo_modeocc=cell(2,1);
+
+for ii=1:2
+    %% Halo capture
+    % capture BECs at poles
+    [this_bec_cent,this_bool_bec]=capture_bec(zxy,configs.bec{ii}.pos,configs.bec{ii}.Rmax,0);
+    
+    % pop bec + thermal counts
+    % bool_bec_combined=cellfun(@(x,y)x|y,bool_bec(:,1),bool_bec(:,2),'UniformOutput',false);
+    this_bool_bec_combined=cell_horzcat(this_bool_bec);       % horizontal concat the boolean list
+    this_bool_bec_combined=cellfun(@(x)sum(x,2)>0,this_bool_bec_combined,'UniformOutput',false);    % row-wise OR
+    
+    this_halo_zxy=cellfun(@(ZXY,BOOL)ZXY(~BOOL,:),zxy,this_bool_bec_combined,'UniformOutput',false);
+    
+    % halo centre: midpoint of 'BEC poles'
+    this_halo_cent=cellfun(@(c1,c2)(c1+c2)/2,this_bec_cent(:,1),this_bec_cent(:,2),'UniformOutput',false);
+    
+    % centre to halo
+    this_halo_zxy0=cellfun(@(zxy,c0)zxy-repmat(c0,[size(zxy,1),1]),this_halo_zxy,this_halo_cent,'UniformOutput',false);
+    
+    
+    %% radial capture (spherical shell)
+    % distance from halo centre
+    this_r0=cellfun(@(x)sqrt(sum(x.^2,2)),this_halo_zxy0,'UniformOutput',false);
+    this_rlim=configs.halo{ii}.R{1}*(1+configs.halo{ii}.dR{1}*[-1,1]);       % radial limits for sph-shell capture
+    
+    this_bool_halo=cellfun(@(r)(r<this_rlim(2))&(r>this_rlim(1)),this_r0,'UniformOutput',false);    % determine halo points
+    
+    % capture halo!
+    this_halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(BOOL,:),this_halo_zxy0,this_bool_halo,'UniformOutput',false);
+    
+    % remove caps
+    this_dz_poles=cellfun(@(c1,c2)abs(c1(1)-c2(1)),this_bec_cent(:,1),this_bec_cent(:,2),'UniformOutput',false);
+    this_dz_poles=mean(vertcat(this_dz_poles{:}));    % pole-pole distance (BEC) in Z
+    
+    this_bool_zcap=cellfun(@(zxy)abs(zxy(:,1))>(0.5*this_dz_poles*configs.halo{ii}.zcap),this_halo_zxy0,'UniformOutput',false);
+    
+    this_halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(~BOOL,:),this_halo_zxy0,this_bool_zcap,'UniformOutput',false);
+    
+    % remove zero counts
+    % TODO - need to remove these shots from analysis!
+    this_bool_empty_halo=(cellfun(@(x)isequal(size(x,1),0),this_halo_zxy0));
+    this_halo_zxy0=this_halo_zxy0(~this_bool_empty_halo);
+    
+    %% Cull aliased hits
+    % cull from centred zxy0 halo data
+    alias_deadz=100e-9*configs.misc.vel_z;      % dZ in 100 ns
+    this_bool_alias=cellfun(@(x) findalias(x,alias_deadz),this_halo_zxy0,'UniformOutput',false);
+    this_halo_zxy0_filt=cellfun(@(x,y) x(~y,:),this_halo_zxy0,this_bool_alias,'UniformOutput',false);
+    % TODO - problem when halo_zxy0 empty
+    
+    % TODO - better var management
+    this_halo_zxy0_orig=this_halo_zxy0;       % store original
+    this_halo_zxy0=this_halo_zxy0_filt;       % alias filtered
+    
+    
+    %% Elipsoid fit
+    efit_flag='';
+    [this_ecent,this_erad,this_evecs,this_v,this_echi2]=ellipsoid_fit(circshift(vertcat(this_halo_zxy0{:}),-1,2),efit_flag);
+    
+    
+    %% Tranform to unit sphere (k-space)
+    % Initialise k-vector in XYZ coord
+    this_halo_k=cellfun(@(x) circshift(x,-1,2),this_halo_zxy0,'UniformOutput',false);
+    
+    % Centre to fitted ellipsoid
+    this_halo_k(:)=boost_zxy(this_halo_k,-this_ecent');
+    
+    % Transform to ellipsoid principal axis (Rotation)
+    M_rot=this_evecs;   % rotation matrix: X'Y'Z'(principal ellipsoid) --> XYZ
+    this_halo_k=cellfun(@(x) (M_rot\x')',this_halo_k,'UniformOutput',false);     % inverse transform
+    
+    % Stretch to UNIT sphere: unit in collision wave-vector/momenta
+    this_halo_k=cellfun(@(x) x./repmat(this_erad',size(x,1),1),this_halo_k,'UniformOutput',false);
+    
+    % Reverse transform to original/detector axis
+    this_halo_k=cellfun(@(x) (M_rot*x')',this_halo_k,'UniformOutput',false);
+    
+    % transform to ZXY system
+    this_halo_k=cellfun(@(x) circshift(x,1,2),this_halo_k,'UniformOutput',false);
+    
+    % plot
+    figure(10);
+    hold on;
+    plot_zxy(this_halo_k,1e5,1,col{ii});
+    axis equal;
+    box on;
+    
+    %% Characterise halo
+    [this_Nsc,this_dk]=halo_characterise(this_halo_k,configs.halo{ii}.zcap,verbose);
+    this_halo_modeocc=halo_mocc(1,0.033,this_Nsc,0.03);
+    
+    %% Halo centering
+    % boost for best g2 BB centering
+    this_halo_k=boost_zxy(this_halo_k,configs.halo{ii}.boost);
+    
+    %% store this mF results
+    halo_k{ii}=this_halo_k;
+    
+    % misc
+    bec_cent{ii}=this_bec_cent;
+    halo_cent{ii}=this_halo_cent;
+    ecent{ii}=this_ecent;
+    erad{ii}=this_erad;
+    evecs{ii}=this_evecs;
+    
+    Nsc{ii}=this_Nsc;
+    dk{ii}=this_dk;
+    halo_modeocc{ii}=this_halo_modeocc;
+end
