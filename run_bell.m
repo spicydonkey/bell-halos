c188a3222b22e66d6f95abbb8cb6a877021fa519
David <dk.shin1992@gmail.com>
2017-09-21 12:11:53 +1000

    smooth corr - theta data with bins

--- run_bell.m
+++ run_bell.m
@@ -52,7 +52,12 @@ dTh_bb=interp2(azim_raw(1:end-1,1:end-1),elev_raw(1:end-1,1:end-1),dTh_bb_raw,az
 % plot interpolated rotation angle
 hfig_dTh_interp=figure();
 plot_sph_surf(azim_g,elev_g,dTh_bb);
-
+title('Interpolated rotation angles');
+axis on;
+xlabel('$K_x$');
+ylabel('$K_y$');
+zlabel('$K_z$');
+colorbar('SouthOutside');
 
 %% process data
 % sort data
@@ -62,23 +67,15 @@ DTh=dTh_bb(:);
 % remove NaNs
 DTh=DTh(~isnan(EE));
 EE=EE(~isnan(EE));
+EE=EE(~isnan(DTh));
+DTh=DTh(~isnan(DTh));
 
 [DTh,Isort]=sort(DTh,'ascend');
 EE=EE(Isort);
 
-%%% smoothing
-% EE=smooth(EE,30);
-% % gaussian convolution
-% sigma=5;
-% sz=10;
-% x = linspace(-sz/2,sz/2,sz);
-% gaussFilter=exp(-x.^2/(2*sigma^2));
-% gaussFilter=gaussFilter/sum(gaussFilter); % normalize
-% EE = conv(EE,gaussFilter,'same');
-
-%%% plot correlation vs theta
-hfig_corr_v_theta=figure(1);
-plot(DTh,EE,'o');
+%%% RAW plot: correlation vs theta
+hfig_corr_v_theta_raw=figure(100);
+plot(DTh,EE,'bo','MarkerSize',3);
 
 % xlim([0,pi]);
 xlim([-pi,pi]);
@@ -91,3 +88,68 @@ hold on;
 line([-pi,0],[1,-1],'Color','r','LineWidth',1.5);
 line([0,pi],[-1,1],'Color','r','LineWidth',1.5);
 hold off;
+box on;
+
+%% smooth correlations
+% Dth is "averaged" angular difference (vs. DTh)
+
+n_Dth_bin=21;
+Dth_edge=linspace(-pi,pi,n_Dth_bin+1);
+Dth_cent=Dth_edge(1:end-1)+0.5*diff(Dth_edge);
+
+Dth=zeros(2,n_Dth_bin);   % mean and serr
+Eth=zeros(2,n_Dth_bin);  
+
+for ii=1:n_Dth_bin
+    bool_bin=(DTh>Dth_edge(ii)&DTh<Dth_edge(ii+1));     % boolean for items in bin
+    n_items=sum(bool_bin);      % number of items in this bin
+    
+    % get values
+    DTh_bin=DTh(bool_bin);
+    EE_bin=EE(bool_bin);
+    
+    % statistics
+    Dth(1,ii)=mean(DTh_bin);
+    Dth(2,ii)=std(DTh_bin)/sqrt(n_items);
+    
+    Eth(1,ii)=mean(EE_bin);
+    Eth(2,ii)=std(EE_bin)/sqrt(n_items);
+end
+
+%%% SMOOTH plot: correlation vs theta
+% misc
+markersize=5;
+linewidth=1.5;
+gray_col=0.5*ones(1,3);         % gray data points
+namearray={'LineWidth','MarkerFaceColor','Color'};      % error bar graphics properties
+valarray={linewidth,'w','b'};                 % 90 deg (normal) data
+
+hfig_corr_v_theta_smooth=figure(101);
+
+%%% DATA
+hcorr=ploterr(Dth(1,:),Eth(1,:),Dth(2,:),Eth(2,:),'o','hhxy',0);
+set(hcorr(1),namearray,valarray,'MarkerSize',markersize,'DisplayName','Experiment');	% DATAPOINT
+set(hcorr(2),namearray,valarray,'DisplayName','');                  % Y-err
+set(hcorr(3),namearray,valarray,'DisplayName','');                  % X-err
+
+%%% THEORIES
+% LHV
+hold on;
+hLHV=line([-pi,0,pi],[1,-1,1],'LineStyle','--','Color',gray_col,'LineWidth',linewidth,'DisplayName','LHV');
+hold off;
+
+% QM
+hold on;
+dth_qm=linspace(-pi,pi,100);
+E_qm=-cos(dth_qm);
+hQM=plot(dth_qm,E_qm,'LineStyle','-','Color','k','LineWidth',linewidth,'DisplayName','QM');
+hold off;
+
+% annotate
+uistack(hQM,'bottom');
+uistack(hLHV,'bottom');
+oleg=legend([hcorr(1),hQM,hLHV],'Location','southeast');
+xlabel('$\Delta \theta = \theta_1 - \theta_0$');
+ylabel('$E$');
+xlim([-pi,pi]);
+ylim([-1,1]);
