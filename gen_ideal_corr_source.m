0bd6104b6d62d3145f81c64c9faa4f27a19c0aaf
David <dk.shin1992@gmail.com>
2017-09-28 15:00:40 +1000

    generate ideal correlated source

---
+++ gen_ideal_corr_source.m
@@ -0,0 +1,15 @@
+% Generate ideal correlated source
+% * mf=0,1 halos
+
+n_shot=1e4;             % number of shots
+n_corr_pairs=1e2*ones(n_shot,2);        % number of correlated pairs generated from source
+det_qe=[0.1,0.1];       % detection efficiency
+
+halo_k0=cell(n_shot,2);
+for ii=1:n_shot
+    % correlated source
+%     halo_temp
+    
+    for jj=1:2
+    end
+end
