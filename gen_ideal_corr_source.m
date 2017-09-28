b3429407fa94959e6fad7cb95512f7c9f9e11dbe
David <dk.shin1992@gmail.com>
2017-09-28 18:31:41 +1000

    generate ideal anti-correlated source - function

--- gen_ideal_corr_source.m
+++ gen_ideal_corr_source.m
@@ -1,15 +1,22 @@
 % Generate ideal correlated source
 % * mf=0,1 halos
+%
+% generates halo_k0 ideal source of momentum-spin correlated atoms
+%
 
-n_shot=1e4;             % number of shots
-n_corr_pairs=1e2*ones(n_shot,2);        % number of correlated pairs generated from source
+% configs
+n_shot=1e3;             % number of shots
+n_corr_pairs=4*ones(n_shot,1);        % number of correlated pairs generated from source
 det_qe=[0.1,0.1];       % detection efficiency
 
+% construct ideal distinguishable halo experiment
 halo_k0=cell(n_shot,2);
 for ii=1:n_shot
-    % correlated source
-%     halo_temp
+    % build correlated source for this shot
+    halo_temp=get_rand_usph(n_corr_pairs(ii));      % create user defined number of atoms on u-sphere
     
     for jj=1:2
+        halo_k0{ii,1}=halo_temp;        % mF=0 atoms
+        halo_k0{ii,2}=-halo_temp;       % mF=1 atoms are just BB-pairs
     end
 end
