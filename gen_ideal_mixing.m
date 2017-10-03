f83a30686329714e13cef89614fb1daf83b2803a
David <dk.shin1992@gmail.com>
2017-10-03 18:06:43 +1100

    create ideal mixing map

---
+++ gen_ideal_mixing.m
@@ -0,0 +1,55 @@
+% Generate mixing angle map for ideal local operation
+%
+%
+clear all; clc; close all;
+
+% configs
+fun_localoper=@(th,phi) phi;
+% fun_localoper=@(th,phi) sin(phi);
+
+verbose=1;
+
+nazim=100;
+nelev=50;
+
+%%  
+if ~exist('fun_localoper','var')
+    error('fun_localoper undefined!');
+end
+
+
+% build azim / elev
+avec=linspace(-pi,pi,nazim);            
+evec=linspace(-pi/2,pi/2,nelev);        % pole-to-pole
+
+[azim{1},elev{1}]=meshgrid(avec,evec);      % data typing conforms with main analysis
+
+% proxy for simulated ideal mixing map
+ampRaman_mf={NaN};      % NaN is for simulation
+
+
+%% Evaluate rotation angle from given function
+Theta=cell(2,1);
+Theta{1}=fun_localoper(azim{1},elev{1});    % evaluate state rotation angle
+Theta{2}=Theta{1};      % mf=1 rotates same as mf=0
+
+
+%% plot map of rotation angle
+if verbose>0
+    hfig_ideal_theta=figure();
+    for ii=1:2
+        subplot(1,2,ii);
+        plot_sph_surf(azim{1},elev{1},Theta{ii});
+        
+        axis on;
+        xlabel('$K_x$');
+        ylabel('$K_y$');
+        zlabel('$K_z$');
+        cbar=colorbar('SouthOutside');
+        cbar.Label.String='$\theta$';
+        
+        title(sprintf('$m_F=$%d',ii-1))
+        cbar.TickLabelInterpreter='latex';
+        cbar.Label.Interpreter='latex';
+    end
+end
