cae74f41bc9f624150b65e22f669e58dddd872ec
David <dk.shin1992@gmail.com>
2017-10-04 13:29:56 +1100

    investigate effect of momentum corr alignment

--- gen_ideal_corr_source.m
+++ gen_ideal_corr_source.m
@@ -6,17 +6,36 @@
 
 clear all; clc; close all;
 
-% configs
+%% CONFIGS
+%%% experiment
 n_shot=1e4;             % number of shots
 n_corr_pairs=100*ones(n_shot,1);        % number of correlated pairs generated from source
 det_qe=0.1*ones(1,2);       % detection efficiency
 
-% define local operation
+% local operation
 % TODO - we can even load the phase map from experiment!
-fun_localoper=@(th,phi) phi;
-% fun_localoper=@(th,phi) 0;              % no local operation
+% fun_localoper=@(th,phi) pi*sin(sqrt(76)*th/(2*pi)).*sin(sqrt(7)*phi*(2*pi)/(pi/2));
+% fun_localoper=@(th,phi) pi*sin(20*th).*sin(20*phi);
+fun_localoper=@(th,phi) 2*phi;
+% fun_localoper=@(th,phi) 0*phi;              % no local operation
 
-% construct ideal distinguishable halo experiment
+%%% HALO
+configs.halo{1}.string='$m_F=0$';
+configs.halo{2}.string='$m_F=1$';
+
+% boosts in this case misalign momentum correlator
+configs.halo{1}.boost=zeros(1,3);
+configs.halo{2}.boost=zeros(1,3);
+
+%%% Spherical zones
+configs.zone.nazim=100;
+configs.zone.nelev=50;
+
+configs.zone.binmethod=1;
+configs.zone.binwidth=0.05;
+
+
+%% construct ideal distinguishable halo experiment
 halo_k0=cell(n_shot,2);
 for ii=1:n_shot
     %% build correlated source for this shot
@@ -68,11 +87,12 @@ for ii=1:n_shot
         is_detected=(rand(size(halo_k0{ii,jj},1),1)<det_qe(jj));
         halo_k0{ii,jj}=halo_k0{ii,jj}(is_detected,:);
     end
+    
+    %% misalign halo centers
+    
+    
 end
 % leave empty halos as is
-
-%% load configs
-run('config_idealsource.m');
 
 %% evaluate correlations
 run('eval_bell_corr.m');
