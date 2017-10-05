05207261c616994f5c698dbfb288bbcdf46367d9
David <dk.shin1992@gmail.com>
2017-10-05 16:18:49 +1100

    save outputs of correlations + tidy

--- run_bell_sim.m
+++ run_bell_sim.m
@@ -5,18 +5,19 @@
 OVERRIDE_CONFIG_FLAG=true;
 
 %% general config
-% datetimestr=datestr(datetime,'yyyymmdd_HHMMSS');    % timestamp when function called
-datetimestr='temp';
+datetimestr=datestr(datetime,'yyyymmdd_HHMMSS');    % timestamp when function called
+% datetimestr='temp';
 path_data_dir='C:\Users\HE BEC\bell\temp';
 path_data_bell=sprintf('bell_sim_%s',datetimestr);
 path_data_loop=sprintf('loop_sim_%s',datetimestr);
+path_data_out=sprintf('out_%s',datetimestr);
 
 
 %% BELL TEST
 %%% experiment
-n_shot=1e3;             % number of shots
-n_corr_pairs=50*ones(n_shot,1);        % number of correlated pairs generated from source
-det_qe=1*ones(1,2);       % detection efficiency
+n_shot=1e4;             % number of shots
+n_corr_pairs=30*ones(n_shot,1);        % number of correlated pairs generated from source
+det_qe=0.1*ones(1,2);       % detection efficiency
 
 % momentum width (implemented on scattered)
 % dk_dither_sd=zeros(1,3);
@@ -35,14 +36,14 @@ configs.halo{1}.string='$m_F=0$';
 configs.halo{2}.string='$m_F=1$';
 
 % boosts in this case misalign momentum correlator
-% configs.halo{1}.boost=-1e-2/sqrt(3)*[1,1,1];
-% configs.halo{2}.boost=1e-2/sqrt(3)*[1,1,1];
-configs.halo{1}.boost=[0,0,0];
-configs.halo{2}.boost=[0,0,0];
+configs.halo{1}.boost=-2.5e-2/sqrt(3)*[1,1,1];
+configs.halo{2}.boost=2.5e-2/sqrt(3)*[1,1,1];
+% configs.halo{1}.boost=[0,0,0];
+% configs.halo{2}.boost=[0,0,0];
 
 %%% Spherical zones
-configs.zone.nazim=100;
-configs.zone.nelev=50;
+configs.zone.nazim=141;
+configs.zone.nelev=71;
 
 configs.zone.binmethod=1;
 configs.zone.binwidth=0.05;
@@ -79,11 +80,18 @@ run('run_bell.m');
 
 %% Outputs
 % source mode occupancy
-wbb=prod(dk_dither_sd)^(1/length(dk_dither_sd));
-M_calc=2*(1.1^3)*(wbb^-2);
+wbb_calc=prod(dk_dither_sd)^(1/length(dk_dither_sd));
+M_calc=2*(1.1^3)*(wbb_calc^-2);
 n_calc=mean(n_corr_pairs)/M_calc;
 
 fprintf('Mode occupancy = %0.3g\n',n_calc);
+
+% save outputs + configs
+varstosave={'EE','DTh','EE_abs','DTh_abs',...
+    'n_shot','n_corr_pairs','det_qe','dk_dither_sd','fun_localoper','configs','nazim','nelev',...
+    'n_calc'};
+save(fullfile(path_data_dir,path_data_out),varstosave{:});
+
 
 %% clear override
 clearvars OVERRIDE_CONFIG_FLAG;     % REMOVE OVERRIDE FLAG WHEN DONE
