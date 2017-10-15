1c44ca941a634857c12f2df4b3413279b49d7c91
David <dk.shin1992@gmail.com>
2017-10-15 13:28:11 +1100

    spherical surf plotter deals with azim wrapping

--- plot_sph_surf.m
+++ plot_sph_surf.m
@@ -1,18 +1,25 @@
-function ax=plot_sph_surf(azim,elev,yy)
+function ax=plot_sph_surf(azim,elev,ff)
 % plots a 2D color-surface on sphere
-% plot_sph_surf(azim,elev,yy)
+% AX = PLOT_SPH_SURF(AZIM,ELEV,FF)
 %
-% azim: meshgrid of azimuthal angles (rad)
-% elev: meshgrid of elevation angles (meas from XY plane) (rad)
-% yy: meshgrid of double array of data at respective point on sphere
+% AZIM: meshgrid of azimuthal angles (rad); ordered and evenly distributed
+% in [-pi,pi]; must be unique modulo 2*pi;
+% ELEV: meshgrid of elevation angles (meas from XY plane) (rad); ordered 
+% and in range [-pi/2,pi/2]; must be unique;
+% FF: meshgrid of function values at (AZIM,ELEV) locations on sphere points
 %
 % ax: graphics object
 
+% wrap angles to close surface
+azim(end+1,:)=azim(1,:)+2*pi;
+elev(end+1,:)=elev(1,:);
+ff(end+1,:)=ff(1,:);
+
 % sph-pol --> cartesian: can use surf!
 r=1;    % mapped on a unit sphere
-[X,Y,Z]=sph2cart(azim,elev,r);
+[xx,yy,zz]=sph2cart(azim,elev,r);
 
-hs=surf(X,Y,Z,yy);
+hs=surf(xx,yy,zz,ff);
 
 set(hs,'FaceAlpha',0.7);
 
