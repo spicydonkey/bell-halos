29bf4d04a9bbf29f321aa546d89b9860bf39e1c8
David <dk.shin1992@gmail.com>
2017-10-16 00:03:30 +1100

    new function - mode counting radially

---
+++ countRadMode.m
@@ -0,0 +1,16 @@
+function ww_rad = countRadMode(vs,rr,mode_thphi,sigR,limR)
+%
+% WW_RAD = COUNTRADMODE(VS,RR,MODE,SIGR,LIMR)
+%
+% WW_RAD: Mx1 weighted sum of counts in the radially resolved momentum mode
+%
+% VS: N-by-3 array of spherical vector of counts
+% RR: Mx1 array of mode norms (modes colinear)
+% MODE_THPHI: 1x2 array (lon,lat) defining mode direction
+% SIGR: sigma for gaussian weights in radial dim
+% LIMR: standard score delimiting outlier
+%
+
+
+
+end
