bee4ba3458488b88ef554e4bafe0157d2de3e8bb
David <dk.shin1992@gmail.com>
2017-11-02 15:58:24 +1100

    untidy hack to plotFlatMap

--- plotFlatMap.m
+++ plotFlatMap.m
@@ -6,20 +6,28 @@ function h = plotFlatMap(lat,lon,Z,axesOpts)
 % LAT, LON should be in degrees
 %
 
+% TODO
+% [] tidy switch cases below
+% [] wrap azimuthal angle (currently chopped)
+% [] create a wrapper for current zone convention (azim,elev) in rads
+%
+
 % axes type
 switch axesOpts
     case 'eckert4'
         axesm eckert4;
         framem; gridm;
         axis off;
+        
+        h=geoshow(lat,lon,Z,'DisplayType','texturemap');
     case 'rect'
         xlim([-180,180]);
         ylim([-90,90]);
+        
+        h=geoshow(lat,lon,Z,'DisplayType','texturemap');
     otherwise
         warning('axesOpts should be set to either "eckert4" or "rect".');
 end
-
-h=geoshow(lat,lon,Z,'DisplayType','texturemap');
 
 % default annotations
 colormap('magma');
