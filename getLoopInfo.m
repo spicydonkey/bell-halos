0a712e181c8c0f6718aff1fba523a8dfc0acfcae
David <dk.shin1992@gmail.com>
2017-11-06 23:40:38 +1100

    hack to incorp delay loop analysis pipeline

--- getLoopInfo.m
+++ getLoopInfo.m
@@ -1,8 +1,11 @@
-function [mf,ampRaman,nver] = getLoopInfo(pathdir)
+function [mf,ampRaman,nver,tdelay] = getLoopInfo(pathdir)
 % [MF, AMPRAMAN, NVER] = GETLOOPINFO(PATHDIR)
 %
 % Gets details about local operation data from name of database directory 
 %
+
+% TODO
+% [] output should be varargout - polymorphic?
 
 % initialise - this is the error state
 mf=NaN;
@@ -10,12 +13,20 @@ ampRaman=NaN;
 nver=NaN;
 
 params=strsplit(pathdir,'_');
-if length(params)~=3
+if length(params)<3
+    % file isn't a valid Loop directory. returns NaN
     return;
+elseif length(params)==3
+    % for MF/RAMANAMP/VERS type
+    mf=str2double(params{1});
+    ampRaman=str2double(params{2});
+    nver=str2double(params{3}(2:end));
+elseif length(params)==4
+    % for MF/RAMANAMP/DELAY/VERS type
+    mf=str2double(params{1});
+    ampRaman=str2double(params{2});
+    nver=str2double(params{3}(2:end));
+    tdelay=str2double(params{4});
 end
-
-mf=str2double(params{1});
-ampRaman=str2double(params{2});
-nver=str2double(params{3}(2:end));
 
 end
