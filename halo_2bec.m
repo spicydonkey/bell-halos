134175985e6de678ae8e20e03b495a51af813826
David <dk.shin1992@gmail.com>
2017-11-09 12:21:41 +1100

    halo capture pipeline: liberal first stage

--- halo_2bec.m
+++ halo_2bec.m
@@ -11,7 +11,16 @@ function [halo_k,bec_cent]=halo_2bec(zxy,p_bec1,p_bec2,r_bec,r_th,dR_halo,zcap,v
 % TODO
 % [] improve algorithm
 % 1. capture BEC: get BEC positions ==> get approx to halo center
-% 2. 
+% 2. 1st stage halo capture
+%   2.1. liberal filtering of counts: radial, BEC, thermal
+%   2.2. ellipsoid mapping --> unit sphere
+% 3. clean halo
+%   3.1. radial filter
+%   3.2. pole filter
+%
+
+% - kdR_hicap: factor to make first stage radial culling more liberal
+
 
 % parse input
 if ~exist('verbose','var')
@@ -19,55 +28,88 @@ if ~exist('verbose','var')
     verbose=0;
 end
 
+%% Configs 
+kdR_hicap=2;
 
-%% Capture BECs and locate halo center
+
+%% 1. Capture marker BECs
 % capture BEC to mark halo poles
 [bec_cent,bool_bec]=capture_bec(zxy,{p_bec1,p_bec2},{r_bec,r_bec},verbose);
 
-% pop bec + thermal counts
+% get atoms in BEC
 % bool_bec_combined=cellfun(@(x,y)x|y,bool_bec(:,1),bool_bec(:,2),'UniformOutput',false);
-bool_bec_combined=cell_horzcat(bool_bec);       % horizontal concat the boolean list
+bool_bec_combined=cell_horzcat(bool_bec);       % boolean array to indicate which BEC atoms belongs to
 bool_bec_combined=cellfun(@(x)sum(x,2)>0,bool_bec_combined,'UniformOutput',false);    % row-wise OR
+% boolean for: is this atom from a BEC?
 
-halo_zxy=cellfun(@(ZXY,BOOL)ZXY(~BOOL,:),zxy,bool_bec_combined,'UniformOutput',false);
-
-% halo centre: midpoint of 'BEC poles'
+%% 2. First stage halo capture
+%%% 2.1. Locate halo center 
+% get halo centre: midpoint of 'BEC poles'
 halo_cent=cellfun(@(c1,c2)(c1+c2)/2,bec_cent(:,1),bec_cent(:,2),'UniformOutput',false);
-% halo radius
+
+% approximate halo radius: 0.5 * distance(BEC1,BEC2)
 R_halo=cellfun(@(c1,c2) sqrt(sumsqr((c1-c2)/2)),bec_cent(:,1),bec_cent(:,2));
 R_halo_mean=mean(R_halo);
 R_halo_std=std(R_halo);
 
-% centre to halo
+%%% 2.2. first capture of halo counts
+% get counts in halo by popping BEC from raw
+halo_zxy=cellfun(@(ZXY,BOOL)ZXY(~BOOL,:),zxy,bool_bec_combined,'UniformOutput',false);
+
+% roughly center the halo to each shot-wise evaluated halo center
 halo_zxy0=cellfun(@(zxy,c0)zxy-repmat(c0,[size(zxy,1),1]),halo_zxy,halo_cent,'UniformOutput',false);
 
-
-%% radial capture
-% distance from halo centre
+%%% 2.3. filter halo
+%%%% radial
+% distance from approx halo centre
 r0=cellfun(@(x)sqrt(sum(x.^2,2)),halo_zxy0,'UniformOutput',false);
-rlim=R_halo_mean*(1+dR_halo*[-1,1]);       % radial limits for sph-shell capture
+rlim_hicap=R_halo_mean*(1+dR_halo*kdR_hicap*[-1,1]);       % radial limits for sph-shell capture
 
-bool_halo=cellfun(@(r)(r<rlim(2))&(r>rlim(1)),r0,'UniformOutput',false);    % determine halo points
+bool_halo_r_hicap=cellfun(@(r)(r<rlim_hicap(2))&(r>rlim_hicap(1)),r0,'UniformOutput',false);    % atoms in radial limits
 
-% capture halo!
-halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(BOOL,:),halo_zxy0,bool_halo,'UniformOutput',false);
+% TODO: keep regional IN bool-array from each filtering stage, AND it when the
+% filtered halo is needed
+% % capture halo!
+% halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(BOOL,:),halo_zxy0,bool_halo_r_hicap,'UniformOutput',false);
 
-% remove caps
-% TODO - try removing the caps after ellipsoid fit
-dz_poles=cellfun(@(c1,c2)abs(c1(1)-c2(1)),bec_cent(:,1),bec_cent(:,2),'UniformOutput',false);
-dz_poles=mean(vertcat(dz_poles{:}));    % pole-pole distance (BEC) in Z
+%%%% elevation (TODO)
+% removing caps using Z complicates angular analysis: use elevation angles
+% instead
+% % remove caps
+% % TODO - try removing the caps after ellipsoid fit
+% dz_poles=cellfun(@(c1,c2)abs(c1(1)-c2(1)),bec_cent(:,1),bec_cent(:,2),'UniformOutput',false);
+% dz_poles=mean(vertcat(dz_poles{:}));    % pole-pole distance (BEC) in Z
+% 
+% bool_zcap=cellfun(@(zxy)abs(zxy(:,1))>(0.5*dz_poles*zcap),halo_zxy0,'UniformOutput',false);
+% 
+% halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(~BOOL,:),halo_zxy0,bool_zcap,'UniformOutput',false);
 
-bool_zcap=cellfun(@(zxy)abs(zxy(:,1))>(0.5*dz_poles*zcap),halo_zxy0,'UniformOutput',false);
+%%%% thermal (TODO)
+% centered around BEC locations but radius is ~2-3 times larger than used
+% for BEC?
 
-halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(~BOOL,:),halo_zxy0,bool_zcap,'UniformOutput',false);
+%%%% Filtered halo
+% TODO - this is nasty joining cells like this. user must supply how many
+% filters used: n_halo_filt1
+% combine different filters
+n_halo_filt1=1;
+bool_halo_filt1_joined=cell(size(bool_halo_r_hicap,1),n_halo_filt1);
+bool_halo_filt1_joined(:,1)=bool_halo_r_hicap;
 
+% get first stage filter
+bool_halo_filt1=cell_horzcat(bool_halo_filt1_joined);
+bool_halo_filt1=cellfun(@(x) all(x,2),bool_halo_filt1,'UniformOutput',false);
 
-%% Elipsoid fit
+% apply the filter
+halo_zxy0=cellfun(@(ZXY,BOOL)ZXY(BOOL,:),halo_zxy0,bool_halo_filt1,'UniformOutput',false);
+
+%% 3. Demanipulate halo
+%%% 3.1. Ellipsoid fit
 efit_flag='';
 [ecent,erad,evecs,ev,echi2]=ellipsoid_fit(circshift(vertcat(halo_zxy0{:}),-1,2),efit_flag);
 
-
-%% Tranform to unit sphere (k-space)
+%%% 3.2. Unit sphere mapping 
+%%%% Tranform to unit sphere (k-space)
 % Initialise k-vector in XYZ coord
 halo_k=cellfun(@(x) circshift(x,-1,2),halo_zxy0,'UniformOutput',false);
 
