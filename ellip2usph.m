c7b93fc2ea435abd40d4c513bd5aeb8d7f9c15fb
David <dk.shin1992@gmail.com>
2017-11-09 14:23:10 +1100

    new packaged function: ellip2usph.m - array friendly - the useful procedure is packaged nicely

---
+++ ellip2usph.m
@@ -0,0 +1,33 @@
+function zxy_usph = ellip2usph(zxy,ecent,erad,evec,verbose)
+% ZXY_USPH = ELLIP2USPH(ZXY, ECENT, ERAD, EVECS, VERBOSE)
+%
+% ellipsoid to unit-sphere 3D transform of vectors 
+%
+% see ellipsoid_fit.m for the ellipsoid parameters e___
+% specifically, ellip params are defined in the XYZ coord sys
+%
+% ZXY is a Nx3 array of cartesian vectors (ARRAY)
+%
+
+nvecs=size(zxy,1);
+xyz_temp=circshift(zxy,-1,2);
+
+% centre to ellipsoid
+% TODO - ecent seems to be a column array: a row vect would be better
+xyz_temp=xyz_temp-repmat(ecent',nvecs,1);
+
+% rotate to ellipsoid principal axis coords
+M_rot=evecs;   % rotation matrix: X'Y'Z'(principal ellipsoid) --> XYZ
+xyz_temp=(M_rot\xyz_temp')';    % inverse transform
+
+% map to unit-sphere by scaling along principal axis
+% TODO - erad seems to be a column array: a row vect would be better
+xyz_temp=xyz_temp./repmat(erad',nvecs,1);
+
+% Rotate back to original (detector) axis
+xyz_temp=(M_rot*xyz_temp')';
+
+% we work in the ZXY coord system
+zxy_usph=circshift(xyz_temp,1,2);
+
+end
