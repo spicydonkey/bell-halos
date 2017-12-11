bc42e2fa7dee8dc2882c7a37f084cdedceac0a1a
David <dk.shin1992@gmail.com>
2017-12-12 00:11:26 +1100

    prototype sph-zone dependent rad transform

--- proto/demo_halo_rad_scaling.m
+++ proto/demo_halo_rad_scaling.m
@@ -15,12 +15,16 @@ dpsi=0.33;
 r_ed=linspace(0.8,1.2,100);
 
 %% main
-kk=vertcat(K{1:1000,1});        % sample of mf=0 halo
+mm=2;
 
+KK=K(1:1000,mm);         % sample data for prototyping
+kk=vertcat(KK{:});
+
+%%% sph-zone radial profiling
 % get normed r-profile
 [nr,rc]=get_halo_sph_r_dist(kk,az,el,dpsi,r_ed);
 % halo caps to NaN
-b_poles=abs(el)>asin(0.8);
+b_poles=abs(el)>asin(0.8);      % 2d boolean array to poles
 % nr(repmat(b_poles,[1,1,size(nr,3)]))=NaN;
 
 % get peak radius at each zone
@@ -39,4 +43,34 @@ figure();
 plotFlatMapWrappedRad(az,el,r0,'eckert4');
 title('peak radius around halo');
 
-% %% transform
+%%% transform
+KK_new=KK;
+for ii=1:numel(KK)
+    ts=zxy2sphpol(KK{ii});
+    ts_new=ts;
+    
+    % th-phi dependent radial scaling
+    for jj=1:size(ts,1)
+        % get sph-pol zone for this count
+        %   simple nearest point
+        tth=ts(jj,1);       % this count azim angle
+        tphi=ts(jj,2);      % this count elev angle
+        [~,idx_th]=min(abs(vaz-tth));
+        [~,idx_phi]=min(abs(vel-tphi));
+        
+        % get normalising radius
+        tr0=r0(idx_th,idx_phi);     
+        
+        % transform this vector
+        ts_new(jj,3)=ts(jj,3)/tr0;
+    end
+    
+    % update new KK in cartesian 
+    KK_new{ii}=sphpol2zxy(ts_new);
+end
+
+% summary
+figure();
+plot_zxy(KK_new,1e4,10);
+axis equal;
+title('sph-zone radially transformed halo');
