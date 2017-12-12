2a91580928aedfda030669790cc9e0d322257174
David <dk.shin1992@gmail.com>
2017-12-12 23:23:46 +1100

    testing anti-corr in source

---
+++ proto/test_source_bell_corr.m
@@ -0,0 +1,54 @@
+% Testing Bell correlation distribution around source halo
+
+% load data
+load('run_source1_20171212.mat');
+% loads entangled source halos in K
+
+
+%% configure
+% scattered zones
+nAz=100;
+nEl=50;
+
+az=linspace(-pi,pi,nAz+1);
+az=az(1:end-1);
+el=linspace(-pi/2,pi/2,nEl);
+
+[Az,El]=ndgrid(az,el);
+
+% counting
+sig_mode=[0.03,Inf];
+lim_mode=[2,Inf];
+count_mode='gauss';
+
+
+%% get counts in momenta modes
+nShots=size(K,1);
+b_bad=(abs(El)>asin(0.7));      % polar caps culled - bad spherical region
+
+N_halo=cell(1,2);
+for mm=1:2
+    N_halo{mm}=cellfun(@(k) haloZoneCount(k,nAz+1,nEl,...
+        sig_mode,lim_mode,count_mode),K(:,mm),'UniformOutput',false);
+    % gaussian weighted counting
+    
+    N_halo{mm}=cat(3,N_halo{mm}{:});   % concatenate cell-array (shots) to matrix
+    
+    N_halo{mm}(repmat(b_bad,[1,1,nShots]))=NaN;   % handle empty polar regions
+end
+
+
+%% Bell correlations
+E=haloBellCorr(N_halo{:});
+
+
+% report summary
+n_nan=sum(sum(isnan(E)));
+E_mean=sum(sum(E,'omitnan'),'omitnan')/(numel(E)-n_nan);
+fprintf('avg E corr = %0.2g\n',E_mean);
+
+
+% visualisation
+figure;
+plotFlatMapWrappedRad(Az,El,E,'eckert4');
+colorbar('SouthOutside');
