ceeaee5bbee059b53bb9545f650e68af4bda8a0f
David <dk.shin1992@gmail.com>
2017-12-12 11:53:48 +1100

    halo sph-density profiler defaults to flat canvas

--- diagnostics/plot_sphdist.m
+++ diagnostics/plot_sphdist.m
@@ -1,10 +1,11 @@
-function [n,az,el]=plot_sphdist(kk,nBins)
+function [n,az,el]=plot_sphdist(kk,nBins,plotmode)
 % Simple diagnostic tool for plotting count distribution around the sphere
 %
 % [nn,az,el]=plot_sphdist(kk,n_az,n_el)
 %
 % kk:       nCounts x 3 vector array (zxy) of counts
 % nBins:    1x2 - number of bins to uniformly divide azimuthal/elevation angles
+% plotmode: string for plotting type flat/3d
 %
 % n:        count density in spherical zone
 % az/el:    2D-grid of azimuthal/elevation angles to spherical zones
@@ -12,9 +13,14 @@ function [n,az,el]=plot_sphdist(kk,nBins)
 
 % parse inputs (defaults)
 if ~exist('nBins','var')
+    warning('nBins is undefined. Setting to default value: [100,50].');
     nBins=[100,50];
 end
-
+if ~exist('plotmode','var')
+    warning('plotmode is undefined. Setting to default value: flat.');
+    plotmode='flat';
+end
+    
 %%% density around sphere
 % defaults
 sig=[0.2,Inf];
@@ -25,11 +31,20 @@ lim=[3,Inf];
 
 
 %%% Plot results
-plot_sph_surf(az,el,n);
+switch plotmode
+    case 'flat'
+        plotFlatMapWrappedRad(az,el,n,'eckert4');
+    case '3d'
+        plot_sph_surf(az,el,n);
+    otherwise
+        error('Unknown plotmode (flat/3d).');
+end
 
 % annotate
-cbar=colorbar('South');
-cbar.Label.String='density [arb u]';
+hcb=colorbar('Southoutside');
+hcb.TickLabelInterpreter='latex';
+hcb.Label.Interpreter='latex';
+hcb.Label.String='density [arb u]';
 
 % configs summary
 %   nBins; sig; lim
