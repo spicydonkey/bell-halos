dc5484a5c346587058b287b662db391d3e4841e8
David <dk.shin1992@gmail.com>
2018-01-11 12:19:59 +1100

    update plotFlatMap codes

--- plotFlatMap.m
+++ plotFlatMap.m
@@ -1,18 +1,18 @@
-function h = plotFlatMap(lat,lon,Z,axesOpts)
-% H = PLOTFLATMAP(LAT,LON,Z,AXESOPTS)
+function h = plotFlatMap(lat,lon,Z,axproj)
+% H = PLOTFLATMAP(LAT,LON,Z,AXPROJ)
 %
 % plot spherical map on a 2D plane
 %
 % LAT, LON should be in degrees
 %
 
-if ~exist('axesOpts','var')
-    warning('axesOpts is undefined. Default to rectangle map.');
-    axesOpts='rect';
+if ~exist('axproj','var')
+    warning('axproj is undefined. Default to rectangle map.');
+    axproj='rect';
 end
 
 % set up axes
-switch axesOpts
+switch axproj
     case 'eckert4'
         axesm eckert4;
         framem; gridm;
@@ -23,7 +23,7 @@ switch axesOpts
         ylim([-90,90]);
         
     otherwise
-        warning('axesOpts should be set to either "eckert4" or "rect".');
+        warning('axproj should be set to either "eckert4" or "rect".');
 end
 % draw data like map
 % h=geoshow(lat,lon,Z,'DisplayType','texturemap');  % NaN as minimum value; smooth
--- plotFlatMapWrappedRad.m
+++ plotFlatMapWrappedRad.m
@@ -1,18 +1,18 @@
-function h = plotFlatMapWrappedRad(Az,El,Z,axesOpts)
-% H = PLOTFLATMATWRAPPEDRAD(AZ,EL,Z,AXESOPTS
+function h = plotFlatMapWrappedRad(Az,El,Z,axproj)
+% H = PLOTFLATMATWRAPPEDRAD(AZ,EL,Z,AXPROJ)
 %
 % AZ: 2d-grid of azimuthal angles (rad); ordered and evenly distributed
 % in [-pi,pi); must be unique modulo 2*pi;
 % EL: 2d-grid of elevation angles (meas from XY plane) (rad); ordered 
 % and in range [-pi/2,pi/2]; must be unique;
 % Z: data defined on grid
-% AXESOPTS: eckert4 or rect
+% AXPROJ: eckert4 or rect
 %
 
 % input checking
-if ~exist('axesOpts','var')
-    warning('axesOpts is undefined. Default to rectangle map.');
-    axesOpts='rect';
+if ~exist('axproj','var')
+    warning('axproj is undefined. Default to rectangle map.');
+    axproj='rect';
 end
 
 % wrap azimuthal angle to close surface
@@ -20,6 +20,6 @@ Az(end+1,:)=Az(1,:)+2*pi;
 El(end+1,:)=El(1,:);
 Z(end+1,:)=Z(1,:);
 
-h=plotFlatMap(rad2deg(El),rad2deg(Az),Z,axesOpts);
+h=plotFlatMap(rad2deg(El),rad2deg(Az),Z,axproj);
 
 end
