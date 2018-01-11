9a077eace22b0b87034f10b0e5860acf66353fbe
David <dk.shin1992@gmail.com>
2018-01-11 12:22:43 +1100

    fruitful density summary of halo

--- summary/summary_disthalo_ndist.m
+++ summary/summary_disthalo_ndist.m
@@ -1,13 +1,23 @@
-function [h]=summary_disthalo_ndist(K)
+function [h,nn,az,el]=summary_disthalo_ndist(K,nBins,plotmode)
 % Summarise distinguishable (2-species) halos with density distribution
 %
-% [h]=summary_disthalo_ndist(K)
+% [h,nn,az,el]=summary_disthalo_ndist(K,OPTS)
 %
 
-%%% configs
+% check args
+if ~exist('nBins','var')
+    warning('nBins is undefined. Setting to default value: [100,50].');
+    nBins=[100,50];
+end
+if ~exist('plotmode','var')
+    warning('plotmode is undefined. Setting to default value: flat.');
+    plotmode='flat';
+end
+
+% configs
 color_mf={'b','r'};
 
-% radial dist
+%%% radial dist
 h(1)=figure();
 for ii=1:2
     hold on;
@@ -16,11 +26,13 @@ for ii=1:2
 end
 drawnow
 
-% sph dist
+%%% sph density dist
 h(2)=figure();
+[az,el]=sphgrid(nBins(1),nBins(2));
+nn=cell(1,2);       % number density (arb) around halo
 for ii=1:2
     subplot(1,2,ii);
-    plot_sphdist(vertcat(K{:,ii}));
+    [nn{ii}]=plot_sphdist(vertcat(K{:,ii}),nBins,plotmode);
 end
 drawnow
 
