d0d2495a79f1f230be4500d61dfdd55d08c9b1ee
David <dk.shin1992@gmail.com>
2018-01-11 17:22:01 +1100

    tidy code

--- wHaloDensity.m
+++ wHaloDensity.m
@@ -1,14 +1,10 @@
-function [nn,z_az,z_el] = wHaloDensity(zxy,nAz,nEl,sig,lim)
+function [nn,az,el] = wHaloDensity(zxy,nAz,nEl,sig,lim)
 % [NN, Z_AZ, Z_EL] = WHALODENSITY(ZXY, NAZ, NEL, SIG, LIM)
 %
 %
 
 % define spherical zones
-az=linspace(-pi,pi,nAz);
-az=az(1:end-1);             % unique angles only
-el=linspace(-pi/2,pi/2,nEl);
-
-[z_az,z_el]=ndgrid(az,el);      % grid to preserve dimensional ordering in array indexing
+[az,el]=sphgrid(nAz,nEl);
 
 % Cart to sph-polar
 vs_sph=zxy2sphpol(zxy);
@@ -16,16 +12,16 @@ clearvars zxy;
 
 % evaluate count density in sphere by the weighted counting algorithm
 nCountsTot=size(vs_sph,1);
-nn=zeros(size(z_az));
-for ii=1:numel(z_az)
-% parfor ii=1:numel(z_az)       % is slower
-    tzone=[z_az(ii),z_el(ii),1];
+nn=zeros(size(az));
+for ii=1:numel(az)
+% parfor ii=1:numel(az)       % slower
+    tzone=[az(ii),el(ii),1];
     tww=weightedCountSph(vs_sph,tzone,sig,lim);
     nn(ii)=tww;
 end
 
 % normalise weighted count density
-wNormFactor=nCountsTot*normSphCount(nn,z_az,z_el);
+wNormFactor=nCountsTot*normSphCount(nn,az,el);
 nn=nn*wNormFactor;
 
 
