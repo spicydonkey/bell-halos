8a695dff3af6ad600e8bff35007f051a7545e840
David <dk.shin1992@gmail.com>
2018-02-05 18:19:09 +1100

    improve functionality of plotFlapMap

--- plotFlatMap.m
+++ plotFlatMap.m
@@ -1,14 +1,23 @@
-function h = plotFlatMap(lat,lon,Z,axproj)
+function h = plotFlatMap(lat,lon,Z,axproj,dispType)
 % H = PLOTFLATMAP(LAT,LON,Z,AXPROJ)
 %
 % plot spherical map on a 2D plane
 %
 % LAT, LON should be in degrees
 %
+%   dispType
+%   	'texturemap'    NaN as minimum value; smooth
+%       'surface'       NaN transparent; no smooth
+%
 
-if ~exist('axproj','var')
+% input checking
+if ~exist('axproj','var') || isequal(axproj,[])
     warning('axproj is undefined. Default to eckert4 map.');
     axproj='eckert4';
+end
+if ~exist('dispType','var') || isequal(dispType,[])
+    warning('dispType is undefined. Default to surface.');
+    dispType='surface';
 end
 
 % set up axes
@@ -26,9 +35,7 @@ switch axproj
         warning('axproj should be set to either "eckert4" or "rect".');
 end
 % draw data like map
-% h=geoshow(lat,lon,Z,'DisplayType','texturemap');  % NaN as minimum value; smooth
-h=geoshow(lat,lon,Z,'DisplayType','surface');   % NaN transparent; no smooth
-
+h=geoshow(lat,lon,Z,'DisplayType',dispType);
 
 % default annotations
 colormap('magma');
--- plotFlatMapWrappedRad.m
+++ plotFlatMapWrappedRad.m
@@ -1,5 +1,5 @@
-function h = plotFlatMapWrappedRad(Az,El,Z,axproj)
-% H = PLOTFLATMATWRAPPEDRAD(AZ,EL,Z,AXPROJ)
+function h = plotFlatMapWrappedRad(Az,El,Z,axproj,dispType)
+% H = PLOTFLATMATWRAPPEDRAD(AZ,EL,Z,AXPROJ,dispType)
 %
 % AZ: 2d-grid of azimuthal angles (rad); ordered and evenly distributed
 % in [-pi,pi); must be unique modulo 2*pi;
@@ -10,9 +10,13 @@ function h = plotFlatMapWrappedRad(Az,El,Z,axproj)
 %
 
 % input checking
-if ~exist('axproj','var')
+if ~exist('axproj','var') || isequal(axproj,[])
     warning('axproj is undefined. Default to eckert4 map.');
     axproj='eckert4';
+end
+if ~exist('dispType','var') || isequal(dispType,[])
+    warning('dispType is undefined. Default to surface.');
+    dispType='surface';
 end
 
 % wrap azimuthal angle to close surface
@@ -20,6 +24,6 @@ Az(end+1,:)=Az(1,:)+2*pi;
 El(end+1,:)=El(1,:);
 Z(end+1,:)=Z(1,:);
 
-h=plotFlatMap(rad2deg(El),rad2deg(Az),Z,axproj);
+h=plotFlatMap(rad2deg(El),rad2deg(Az),Z,axproj,dispType);
 
 end
