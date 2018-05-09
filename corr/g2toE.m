0c155c04c6afd6f738b90b5c9400bae3e2073477
David <dk.shin1992@gmail.com>
2018-05-09 15:04:39 +1000

    new function: E from g2 and minor tidying

---
+++ corr/g2toE.m
@@ -0,0 +1,18 @@
+function [E,E0] = g2toE(g2_corr,g2_anti)
+%   spin-correlation evaluated from two-particle g2 correlation amplitudes
+%   calculated between correlated and anti-corr pairs
+%
+%   E   : raw correlation coefficient as determined from g2
+%   E0  : mode occupancy corrected correlation (infinite g2 limit)
+%
+
+P_corr = g2_corr./(g2_corr+g2_anti);
+P_anti = 1-P_corr;
+
+E = P_corr - P_anti;        % measured correlation coefficient
+
+g2=g2_corr+g2_anti-1;           % effective two-particle corr amplitude
+E_ideal_src=(g2-1)/(g2+1);      % ideal source 2-particle correlation
+E0=E/E_ideal_src;               % mode occupancy corrected correlation
+
+end
--- get_rand_usph.m
+++ get_rand_usph.m
@@ -1,4 +1,9 @@
 % Generate a randomly sampled point on a 3D unit sphere
+%   u_xyz = get_rand_usph(N)
+%   
+%   N:  number of points
+%   u_xyz:  Nx3 array of cartesian 3-vectors
+%
 function u_xyz = get_rand_usph(N)
     rand_th_azim=rand(N,1);
     rand_th_elev=rand(N,1);
