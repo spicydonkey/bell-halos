7e772d7ad1dc39376a28f9b32dc1b06938d8fe6b
David <dk.shin1992@gmail.com>
2018-08-17 12:39:13 +1000

    return graphical objects from pretty surf function

--- img/surf_g2_2d.m
+++ img/surf_g2_2d.m
@@ -1,4 +1,4 @@
-function surf_g2_2d(g2,dk,idx_dk0)
+function [h,s]=surf_g2_2d(g2,dk,idx_dk0)
 %   g2 should be a 3-dim array defined on
 %   dk a 1x3 cell array of Z,X,Y grid points
 %   idx_dk0 is index of dim @ dk=0 in that component
