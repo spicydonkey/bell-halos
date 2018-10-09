6176a97cbded8882ab2bb67cefcd62b22e2e7396
David <dk.shin1992@gmail.com>
2018-10-09 11:46:47 +1100

    FIX: g2 stderr from bootstrap is fixed

--- analysis/misc/fix_bootstrap_analysis.m
+++ analysis/misc/fix_bootstrap_analysis.m
@@ -11,11 +11,20 @@ path_old_data=fullfile(path_dir,old_data);
 % load data
 load(path_old_data);    % load into workspace
 
-% correct SE estimation by bootstrapping (scaled by sampling fraction)
+%% correct SE estimation by bootstrapping (scaled by sampling fraction)
 for ii=1:numel(S)
-    t_se_bs_old=S(ii).E_bootstrap_sdev;     % unscaled error estimate
     t_samp_frac=S(ii).n_frac_samp;          % bs sampling fraction
     
+    % correlator
+    t_se_bs_old=S(ii).E_bootstrap_sdev;     % unscaled error estimate
     S(ii).E_bootstrap_sdev_old=t_se_bs_old;         % store the old error
     S(ii).E_bootstrap_sdev=sqrt(t_samp_frac)*t_se_bs_old;    % sample size corrected
+    
+    % g2 stderr
+    S(ii).g2_sdev_old=cell(numel(S(ii).g2_sdev),1);
+    for jj=1:numel(S(ii).g2_sdev)
+        S(ii).g2_sdev_old{jj}=S(ii).g2_sdev{jj};
+        S(ii).g2_sdev{jj}=cellfun(@(x) sqrt(t_samp_frac)*x,S(ii).g2_sdev{jj},'UniformOutput',false);
+    end
 end
+
