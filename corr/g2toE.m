0f71a5cb390bc459dcbbcd86f0542cd3d0a7eeb4
David <dk.shin1992@gmail.com>
2018-10-17 17:31:04 +1100

    minor debug

--- corr/g2toE.m
+++ corr/g2toE.m
@@ -12,7 +12,7 @@ P_anti = 1-P_corr;
 E = P_corr - P_anti;        % measured correlation coefficient
 
 g2=g2_corr+g2_anti-1;           % effective two-particle corr amplitude
-E_ideal_src=(g2-1)/(g2+1);      % ideal source 2-particle correlation
-E0=E/E_ideal_src;               % mode occupancy corrected correlation
+E_ideal_src=(g2-1)./(g2+1);      % ideal source 2-particle correlation
+E0=E./E_ideal_src;               % mode occupancy corrected correlation
 
 end
