924c1b29f9419a8f8f759a4796bb5188fc68d934
David <dk.shin1992@gmail.com>
2018-10-30 18:57:18 +1100

    analysis for all regions around sphere + model fit and vis

--- analysis/exp4_bell_t_evo/src/main_tevo.m
+++ analysis/exp4_bell_t_evo/src/main_tevo.m
@@ -7,18 +7,28 @@
 fdata='C:\Users\HE BEC\Dropbox\phd\data\bell_epr_2018\proc\exp4_tevo\exp4_20181029.mat';
 
 % capture region
-alpha=pi/8;     % cone half-angle
-n_az=12;         % azimuthally equispaced between 0 and pi (excl)
-n_el=7;         % elev equispaced between -maxphi and maxphi
-phi_max=asin(0.75);
+alpha=pi/12; % pi/8;     % cone half-angle
+lim_az=[-pi,pi];            % limit of azim angle (exc max lim)
+n_az=24;                    % equispaced bins
+phi_max=pi/3;
+lim_el=[-phi_max,phi_max];
+n_el=9;
+
+az_disp=deg2rad([0,45,90]);     % azim sections (great circles) to display
 
 % g2
 n_dk=15;
 lim_dk=[-0.2,0.2];
+% n_dk=7;
+% lim_dk=[-0.1,0.1];
 
 % bootstrapping
 bs_frac=0.2;
-bs_nrep=20;        %20;
+bs_nrep=20;
+% bs_nrep=1;
+
+% He*
+C_gymag=2.8e6;     % gyromagnetic ratio (gamma) [Hz/G]
 
 % vis
 f_units='normalized';
@@ -45,14 +55,21 @@ load(fdata);
 warning('Reducing data for speed.');
 k_tau{1}=k_tau{1}(1:3000,:);
 
+% warning('DEBUG ONLY!!!!');
+% k_tau=cellfun(@(k) k(1:100,:),k_tau,'UniformOutput',false);
+
+
 %% create halo sections to investigate
-Vaz=linspace(0,pi,n_az+1);
-Vaz=Vaz(1:end-1);
+Vaz=linspace(lim_az(1),lim_az(2),n_az+1);
+Vaz=Vaz(1:end-1);       % exclude last
+Vel=linspace(lim_el(1),lim_el(2),n_el);
 
-Vel=linspace(-phi_max,phi_max,n_el);
-
-[vaz,vel]=ndgrid(Vaz,Vel);    % pairs
+[vaz,vel]=ndgrid(Vaz,Vel);    % AZ-EL grid
 n_zone=numel(vaz);
+
+% special angles
+[~,iaz_disp]=arrayfun(@(x) min(abs(Vaz-x)),az_disp);     % idx to ~displayable azim angles
+[~,iel_0]=min(abs(Vel));        % idx to ~zero elev angle (equator)
 
 %% create g2 params
 dk_ed_vec=linspace(lim_dk(1),lim_dk(2),n_dk+1);
@@ -85,7 +102,7 @@ B_bs_se=m3;
 B0_bs_se=m3;
 
 for ii=1:n_tau
-    %% 
+    %%
     k=k_tau{ii};
     n_data_size=size(k,1);
     
@@ -172,7 +189,7 @@ mdl_tevo.fopt=statset('TolFun',10^-10,'TolX',10^-10,'MaxIter',10^6,'UseParallel'
 par0=1;
 
 % preproc
-tau0=tau-tau(1);        % time evolution since T(PSI+)~0.8 ms
+tau0=tau-tau(1);        % time evolution since T(PSI+)~0.8 [ms]
 tau0_fit=linspace(min(tau0),max(tau0),1e3);
 
 % fit
@@ -187,7 +204,14 @@ end
 
 % eval fitted model
 mdl_tevo.fit_par=cellfun(@(m) m.Coefficients.Estimate,mdl_tevo.fit);
+mdl_tevo.fit_par_se=cellfun(@(m) m.Coefficients.SE,mdl_tevo.fit);
 B0_fit=cellfun(@(f) feval(f,tau0_fit),mdl_tevo.fit,'UniformOutput',false);
+
+% diff B field 
+om_fit=1e3*mdl_tevo.fit_par;        % fitted omega [rad/s]
+om_se_fit=1e3*mdl_tevo.fit_par_se;
+deltaB=2*pi*om_fit/C_gymag;        % diff in B-field strength [G]
+deltaB_se=2*pi*om_se_fit/C_gymag;
 
 
 %% vis: corrected parity - ALL
@@ -218,11 +242,8 @@ ax.LineWidth=ax_lwidth;
 ylim([-1.2,1.2]);
 
 %% vis: Polar distribution
-% config
-iaz_disp=[1,4,7];
-
 for ii=1:length(iaz_disp)
-    iaz=iaz_disp(ii);       % azim idx to great circle
+    iaz=iaz_disp(ii);       % azim idx to displayable great circle
     taz=Vaz(iaz);
     figname=sprintf('B0_tevo_polar_%0.0f',rad2deg(taz));
     
@@ -233,7 +254,6 @@ for ii=1:length(iaz_disp)
     
     pleg=NaN(n_el,1);
     for jj=1:n_el
-%         tp=ploterr(tau,squeeze(B0(:,iaz,jj)),[],squeeze(B0_bs_se(:,iaz,jj)),'-o','hhxy',0);
         tp=ploterr(tau,squeeze(B0(:,iaz,jj)),[],squeeze(B0_bs_se(:,iaz,jj)),'o','hhxy',0);
         set(tp(1),'MarkerSize',mark_siz,'LineWidth',line_wid,...
             'MarkerFaceColor',ccl(jj,:),'Color',cc(jj,:),'DisplayName',num2str(rad2deg(Vel(jj)),2));
@@ -262,20 +282,13 @@ for ii=1:length(iaz_disp)
 end
 
 %% vis: Equatorial distribution
-% config
-[~,iel_0]=min(abs(Vel));
-el_0=Vel(iel_0);
-
 figname='B0_tevo_eqt';
-
-% figure
 figure('Name',figname,...
     'Units',f_units,'Position',f_pos_wide,'Renderer',f_ren);
 hold on;
 
 pleg=NaN(n_az,1);
 for ii=1:n_az
-%     tp=ploterr(tau,squeeze(B0(:,ii,iel_0)),[],squeeze(B0_bs_se(:,ii,iel_0)),'-o','hhxy',0);
     tp=ploterr(tau,squeeze(B0(:,ii,iel_0)),[],squeeze(B0_bs_se(:,ii,iel_0)),'o','hhxy',0);
     set(tp(1),'MarkerSize',mark_siz,'LineWidth',line_wid,...
         'MarkerFaceColor',ccl(ii,:),'Color',cc(ii,:),'DisplayName',num2str(rad2deg(Vaz(ii)),2));
@@ -299,6 +312,108 @@ ylabel('Parity $\bar{\mathcal{B}}_{\pi/2}$');
 ax.FontSize=fontsize;
 ax.LineWidth=1.2;
 ylim([-1.2,1.2]);
-title('Equator ($\phi=0$)');
+titlestr=sprintf('Equator (%s %0.2g)','$\phi=$',Vel(iel_0));
+title(titlestr);
 lgd=legend(pleg,'Location','EastOutside');
 title(lgd,'Azimuth $\theta$ (deg)');
+
+
+%% vis: B0 distribution (3D)
+H=[];
+for ii=1:n_tau
+    %%
+    figname=sprintf('B0_sphdist_3d_%0.2f',tau(ii));
+    H(ii)=figure('Name',figname,'Units',f_units,'Position',f_pos,'Renderer',f_ren);
+    tB0=squeeze(B0(ii,:,:));
+    tp=plot_sph_surf(vaz,vel,tB0);
+    
+    % annotation
+%     tp.FaceAlpha=1;
+
+    titlestr=sprintf('%s %0.2f ms','$\tau=$',tau(ii));
+    title(titlestr);
+
+    ax=gca;
+    ax.FontSize=fontsize;
+    
+    axis on;
+    box on;
+    xlabel('$k_x$');
+    ylabel('$k_y$');
+    zlabel('$k_z$');
+    xlim([-1,1]);
+    ylim([-1,1]);
+    zlim([-1,1]);
+    
+    cbar=colorbar();
+    caxis([-1,1]);
+    cbar.Limits=[-1,1];
+    cbar.TickLabelInterpreter='latex';
+    cbar.Label.Interpreter='latex';
+    cbar.Label.String='Parity $\bar{\mathcal{B}}_{\pi/2}$';
+    cbar.Label.FontSize=fontsize;
+end
+
+%% vis: B0 distribution (2D)
+H=[];
+for ii=1:n_tau
+    %%
+    figname=sprintf('B0_sphdist_2d_%0.2f',tau(ii));
+    H(ii)=figure('Name',figname,'Units',f_units,'Position',f_pos,'Renderer',f_ren);
+    tB0=squeeze(B0(ii,:,:));
+    tp=plotFlatMap(rad2deg(vel),rad2deg(vaz),tB0,'eckert4','texturemap');
+
+    % annotation
+    titlestr=sprintf('%s %0.2f ms','$\tau=$',tau(ii));
+    title(titlestr);
+
+    ax=gca;
+    ax.FontSize=fontsize;
+    
+    cbar=colorbar('southoutside');
+    caxis([-1,1]);
+    cbar.Limits=[-1,1];
+    cbar.TickLabelInterpreter='latex';
+    cbar.Label.Interpreter='latex';
+    cbar.Label.String='Parity $\bar{\mathcal{B}}_{\pi/2}$';
+    cbar.Label.FontSize=fontsize;
+end
+
+%% vis: deltaB (asymmetry measure) around halo: t-indep model (3D SPH)
+h=figure('Name','deltaB_sphdist_3d','Units',f_units,'Position',f_pos,'Renderer',f_ren);
+
+tp=plot_sph_surf(vaz,vel,1e3*deltaB);
+
+% annotation
+ax=gca;
+ax.FontSize=fontsize;
+
+axis on;
+box on;
+xlabel('$k_x$');
+ylabel('$k_y$');
+zlabel('$k_z$');
+xlim([-1,1]);
+ylim([-1,1]);
+zlim([-1,1]);
+
+cbar=colorbar();
+cbar.TickLabelInterpreter='latex';
+cbar.Label.Interpreter='latex';
+cbar.Label.String='$\Delta \mathrm{B}$ [mG]';
+cbar.Label.FontSize=fontsize;
+
+%% vis: deltaB (asymmetry measure) around halo: t-indep model (2D PROJ MAP)
+h=figure('Name','deltaB_sphdist_2d','Units',f_units,'Position',f_pos,'Renderer',f_ren);
+
+tp=plotFlatMap(rad2deg(vel),rad2deg(vaz),1e3*deltaB,'eckert4','texturemap');
+
+% annotation
+ax=gca;
+ax.FontSize=fontsize;
+
+cbar=colorbar('SouthOutside');
+cbar.TickLabelInterpreter='latex';
+cbar.Label.Interpreter='latex';
+cbar.Label.String='$\Delta \mathrm{B}$ [mG]';
+cbar.Label.FontSize=fontsize;
