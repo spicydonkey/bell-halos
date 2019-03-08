f145a51ad6bce61e898d0b2429108ed0795452cb
David <dk.shin1992@gmail.com>
2019-03-09 00:52:40 +1100

    refactor and tidy code

--- diagnostics/plot_rdist.m
+++ diagnostics/plot_rdist.m
@@ -1,4 +1,4 @@
-function [n,r,b]=plot_rdist(kk,Nbins)
+function [n_r,r_cent,b]=plot_rdist(kk,Nbins)
 % Plot normalised radial histogram
 %
 % [n,r] = plot_rdist(kk,Nbins)
@@ -6,40 +6,22 @@ function [n,r,b]=plot_rdist(kk,Nbins)
 % kk:       nCountsx3 vector array of counts
 % Nbins:    number of bins to divide min-max (default 50)
 %
-% n:        normalised radial count density
-% r:        radial bin centers
+% n_r:        normalised radial count density
+% r_cent:        radial bin centers
+% b: handle to bar plot
 %
 
 % parse inputs
-if ~exist('N','var')
+if ~exist('Nbins','var')
     Nbins=50;
 end
 
-%%% Histogram
+%%% Radial PDF
 knorm=vnorm(kk);      % get vector norms
 
 % build bins for histogram 
-r_ed=linspace(min(knorm),max(knorm),Nbins+1)';
-r=r_ed(1:end-1)+0.5*diff(r_ed);
+r_ed=linspace(min(knorm),max(knorm),Nbins+1);
 
-% do the 1D histogram in vector norms
-Nr=nhist(knorm,{r_ed});
-
-%%% normalise distribution
-% normalise radially dependent bin volume ~ 4*pi*r^2*dr
-vol_r_bin=4*pi*(r.^2).*diff(r_ed);
-n=Nr./vol_r_bin;
-
-% PDF: normalise by total and bin-size
-n=n./(sum(n)*diff(r_ed));
-
-
-%%% plot
-b=bar(r,n,1,'FaceAlpha',0.5);
-
-% annotate
-box on;
-xlabel('r');
-ylabel('PDF');
+[n_r,r_cent,b] = radialprofile(knorm,r_ed,1);
 
 end
