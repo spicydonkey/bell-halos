19c7ed26fbe1cf12f395c559bedffa38f5ccb38c
david <dk.shin1992@gmail.com>
2019-07-12 20:14:43 +1000

    add correlation criteria

--- model/demo_Bcorr_fit.m
+++ model/demo_Bcorr_fit.m
@@ -4,8 +4,8 @@
 % 2019-07-11
 
 %% data
-% data_path = 'C:\Users\David\Dropbox\PhD\projects\bell_witness\paper\fig4\data_20181023.mat';
-data_path = 'C:\Users\hE BEC\Dropbox\PhD\projects\bell_witness\paper\fig4\data_20181023.mat';
+data_path = 'C:\Users\David\Dropbox\PhD\projects\bell_witness\paper\fig4\data_20181023.mat';
+% data_path = 'C:\Users\hE BEC\Dropbox\PhD\projects\bell_witness\paper\fig4\data_20181023.mat';
 
 vars_to_load = {'B','B_se','Th','tau','Tpi','S_epr','S_epr_se','Th_epr'};
 
@@ -61,13 +61,15 @@ S_pred_ci(~b_ok,:) = NaN;
 
 %% Graphics config common
 fig_lwidth=1;
-
+col_gray=0.9*ones(1,3);
 
 
 %% plot: B vs theta
 %%% config
 col_B = 'b';
 col_B_theory = 'k';
+col_bogo=col_gray;
+
 
 %%% plot
 H=figure('Name','B_vs_theta');
@@ -77,7 +79,7 @@ hold on;
 pB_data = ploterr(S.Th/pi,S.B,[],S.B_se,'o','hhxy',0);
 set(pB_data(1),'MarkerEdgeColor',col_B,'MarkerFaceColor','w',...
     'LineWidth',fig_lwidth,'DisplayName','data');
-set(pB_data(2),'Color',col_B);
+set(pB_data(2),'Color',col_B,'LineWidth',fig_lwidth);
 
 % fitted theory curve
 pB_fit = plot(th/pi,B_pred,'-','Color',col_B_theory);
@@ -87,6 +89,7 @@ set(pB_fit,'DisplayName',tstr);
 pB_fit_ci = plot(th/pi,B_pred_ci,'--','Color',col_B_theory);       % confidence interval
 
 % annotation
+ax=gca;
 uistack(pB_fit,'bottom');
 uistack(pB_fit_ci,'bottom');
 
@@ -94,13 +97,28 @@ box on;
 xlabel('Rotation angle $\theta/\pi$');
 ylabel('Correlator $\mathcal{B}(\theta)$');
 % title('Fit of correlator');
-lgd = legend([pB_data(1),pB_fit],'Location','best');
+lgd = legend([pB_data(1),pB_fit],'Location','best','AutoUpdate','off');
 
+
+%%% violation regions
+% Bogoliubov nonlocality signal
+B_bogo_min = 1/sqrt(2);
+p_bogo=patch([ax.XLim(1),ax.XLim(2),ax.XLim(2),ax.XLim(1)],...
+    [B_bogo_min,B_bogo_min,ax.YLim(2),ax.YLim(2)],...
+    col_bogo,...
+    'EdgeColor','none');
+uistack(p_bogo,'bottom');      % this should REALLY be bottom - to not cover any other graphics
+
+% text(0,0.5*(B_bogo_max+1),sprintf('Nonlocal (QM)'),'FontSize',fontsize-1,'VerticalAlignment','middle');
+
+set(ax,'Layer','Top');     % graphics axes should be always on top
 
 %% plot: S vs theta
 %%% config
 col_S = 'r';
 col_S_theory = 'k';
+col_epr = col_gray;
+col_ent = col_gray;
 
 %%% plot
 H=figure('Name','S_vs_theta');
@@ -120,13 +138,41 @@ set(pS_fit,'DisplayName',tstr);
 pS_fit_ci = plot(th/pi,S_pred_ci,'--','Color',col_S_theory);       % confidence interval
 
 % annotation
+ax=gca;
 uistack(pS_fit,'bottom');
 uistack(pS_fit_ci,'bottom');
 
 box on;
 xlabel('Rotation angle $\theta/\pi$');
 ylabel('Steering parameter $\mathcal{S}(\theta,\theta + \pi/2)$');
-lgd = legend([pS_data(1),pS_fit],'Location','best');
+lgd = legend([pS_data(1),pS_fit],'Location','best','AutoUpdate','off');
 
 xlim(1.5*[-1,1]);
 ylim([0,2]);        % algebraic range
+
+
+%%% violation region
+% EPR
+S_epr_min=sqrt(2);
+p_epr=patch([ax.XLim(1),ax.XLim(2),ax.XLim(2),ax.XLim(1)],...
+    [S_epr_min,S_epr_min,2,2],...
+    col_epr,'EdgeColor','none');
+uistack(p_epr,'bottom');
+% hatch to distinguish
+hatchfill2(p_epr,'single','HatchAngle',45,'HatchDensity',30,...
+    'HatchColor','k');      %,'HatchLineWidth',1
+set(gca,'Layer','Top');     % graphics axes should be always on top
+% text(pi/4,0.5*(S_epr_min+2),sprintf('Bell correlation witness'),'FontSize',fontsize-1,...
+%     'HorizontalAlignment','center','VerticalAlignment','middle',...
+%     'BackgroundColor',col_epr);
+
+% entanglement
+S_ent_min = 1;
+p_ent=patch([ax.XLim(1),ax.XLim(2),ax.XLim(2),ax.XLim(1)],...
+    [S_ent_min,S_ent_min,2,2],...
+    col_ent,'EdgeColor','none');
+uistack(p_ent,'bottom');   
+% text(pi/4,0.5*(S_ent_min+S_epr_min),sprintf('Entanglement'),'FontSize',fontsize-1,...
+%     'HorizontalAlignment','center','VerticalAlignment','middle');
+
+set(ax,'Layer','Top');     % graphics axes should be always on top
